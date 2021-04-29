72d11fd918b70f7be751db09f5ad66a46e65f381
AidanSchmid <Aidan.schmid@quinnipiac.edu>
2021-04-29 13:36:07 -0400

    Visuals

--- app/src/main/java/edu/quinnipiac/finalproject4/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/finalproject4/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.quinnipiac.finalproject4;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.view.MenuItemCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.toolbar));
 
 
     }
